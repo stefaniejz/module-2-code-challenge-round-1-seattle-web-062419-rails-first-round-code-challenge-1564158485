927d2f34bc9a885736aa44003b1c2db42893c57e
Meryl Dakin <meryl.k.dakin@gmail.com>
2017-07-31 09:58:03 -0400

    fixed typo "wilder" to "wielder"

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,8 @@ superheroes = [
 ]
 
 superpowers = [
-  {name:"super strength",description: "gives the wilder super human strengths"},
-  {name:"flight",description: "gives the wilder the ability to fly through the skys at supersonic speed"},
+  {name:"super strength",description: "gives the wielder super human strengths"},
+  {name:"flight",description: "gives the wielder the ability to fly through the skys at supersonic speed"},
   {name:"super human senses",description: "allows the person to user their senses at super human level"},
   {name:"elasticity",description: "can stretch the human body to extreme lengths"},
 ]
