ed485bed8721c81572f44bb2aa643c433bb076a7
Maxcell Wilson <maxcell.wilson@gmail.com>
2018-06-14 15:39:22 -0400

    Add Squirrel Girl

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,25 +2,25 @@ Heroine.destroy_all
 Power.destroy_all
 
 powers = [
-  {name:"super strength", description: "gives the wielder super human strengths"},
-  {name:"flight", description: "gives the wielder the ability to fly through the skys at supersonic speed"},
-  {name:"super human senses", description: "allows the person to user their senses at super human level"},
-  {name:"elasticity", description: "can stretch the human body to extreme lengths"},
+  {name:"super strength", description: "gives the wielder super human strengths" },
+  {name:"flight", description: "gives the wielder the ability to fly through the skys at supersonic speed" },
+  {name:"super human senses", description: "allows the person to user their senses at super human level" },
+  {name:"elasticity", description: "can stretch the human body to extreme lengths" },
 ]
 
 powers.each {|power| Power.create(power)}
 
 heroines = [
   {name: "Kamala Khan", super_name: "Ms. Marvel"},
+  {name: "Doreen Green", super_name: "Squirrel Girl" },
   {name:"Gwen Stacy", super_name:"Spider-Gwen" },
   {name:"Janet Van Dyne", super_name:"The Wasp" },
   {name:"Wanda Maximoff", super_name:"Scarlet Witch" },
-  {name:"Natasha Romanoff", super_name:"Black Widow" },
   {name:"Carol Danvers", super_name:"Captain Marvel" },
   {name:"Jean Grey", super_name:"Dark Phoenix" },
   {name:"Ororo Munroe", super_name:"Storm" },
   {name:"Shadowcat", super_name:"Kitty Pryde" },
-  {name:"Elektra", super_name:"Elektra Natchios" }
+  {name:"Elektra", super_name:"Elektra Natchios" },
 ]
 
 # ---------------
